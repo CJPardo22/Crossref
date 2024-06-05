92dc6576ffaab730673652a43ea9aba1f2671e2f
Cristian Pardo <cristianpardo044@gmail.com>
2024-06-05 14:32:06 -0500

    Funcionando Modificación y descarga, problema de reinicio con el input

--- src/XmlForm.jsx
+++ src/XmlForm.jsx
@@ -6,6 +6,7 @@ import useXMLFileStore from "./store/useXMLFileStore";
 export const XmlForm = () => {
   const { xmlContent, setXMLContent } = useXMLFileStore();
   const parsedXML = new XMLParser().parseFromString(xmlContent);
+  console.log("🦁", parsedXML);
   const [modifiedXML, setModifiedXML] = useState(parsedXML);
 
   const handleInputChange = (e, node) => {
@@ -18,7 +19,18 @@ export const XmlForm = () => {
     const updatedXMLString = new XMLParser().toString(modifiedXML);
     setXMLContent(updatedXMLString);
     console.log("Updated XML JSON:", modifiedXML);
-    console.log("🤠", updatedXMLString)
+    console.log("🤠", updatedXMLString);
+  };
+
+  const handleDownload = () => {
+    const updatedXMLString = new XMLParser().toString(modifiedXML);
+    const blob = new Blob([updatedXMLString], { type: "application/xml" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "modified_file.xml";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   };
 
   const renderForm = (jsonData) => {
@@ -56,6 +68,7 @@ export const XmlForm = () => {
     <>
       <div>{renderForm(modifiedXML)}</div>
       <button onClick={handleSave}>Guardar</button>
+      <button onClick={handleDownload}>Descargar</button>
     </>
   );
 };
