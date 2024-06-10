53a8fc18dc757b970b574ea770e8dd1b726c1d65
Cristian Pardo <cristianpardo044@gmail.com>
2024-06-09 20:49:00 -0500

    Proyecto con React Bootstrap y agregando el crossmark funcionando

---
+++ src/AddCrossmark.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+
+export const AddCrossmark = ({ onSave }) => {
+  const [show, setShow] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [isInputEmpty, setIsInputEmpty] = useState(false);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    const trimmedValue = inputValue.trim();
+    if (trimmedValue === "") {
+      setIsInputEmpty(true);
+    } else {
+      onSave(trimmedValue);
+      setInputValue("");
+      setIsInputEmpty(false);
+      setShow(false);
+    }
+  };
+
+  const handleClose = () => {
+    setShow(false);
+    setIsInputEmpty(false);
+  };
+
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Agregar Crossmark
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Agregar Valor del Crossmark</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formCrossmarkValue">
+              <Form.Label>Valor Crossmark:</Form.Label>
+              <Form.Control
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder="Ingrese el valor de Crossmark"
+                isInvalid={isInputEmpty}
+              />
+              <Form.Control.Feedback type="invalid">
+                El valor no puede estar vacío.
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancelar
+          </Button>
+          <Button variant="primary" onClick={handleSubmit}>
+            Agregar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
--- src/XmlForm.jsx
+++ src/XmlForm.jsx
@@ -1,7 +1,10 @@
 import XMLParser from "react-xml-parser";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { AddCrossmark } from "./AddCrossmark";
 import useXMLFileStore from "./store/useXMLFileStore";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export const XmlForm = () => {
   const { xmlContent, setXMLContent } = useXMLFileStore();
@@ -13,6 +16,30 @@ export const XmlForm = () => {
     const newValue = e.target.value;
     node.value = newValue;
     setModifiedXML({ ...modifiedXML });
+  };
+
+  const handleAddCrossmark = (value) => {
+    const newJSON = {...modifiedXML}
+  };
+
+  const addCrossmarkToNode = (node) => {
+    if (node.name === "pages") {
+      const crossmarkNode = {
+        name: "Crossmark",
+        attributes: {},
+        value: value,
+        children: [],
+      };
+      const parentNode = node.parent;
+      if (parentNode && parentNode.children) {
+        const index = parentNode.children.indexOf(node);
+        parentNode.children.splice(index + 1, 0, crossmarkNode);
+      }
+    }
+    if (newJSON.children && newJSON.children.length > 0) {
+      newJSON.children.forEach(addCrossmarkToNode);
+    }
+    setModifiedXML(newJSON);
   };
 
   const handleSave = () => {
@@ -38,12 +65,18 @@ export const XmlForm = () => {
       if (node.name && node.value) {
         return (
           <div key={uuidv4()}>
-            <label>{node.name}</label>
-            <input
+            <Form.Label>{node.name} :</Form.Label>
+            {/* <label>{node.name}</label> */}
+            <Form.Control
               type="text"
               value={node.value}
               onChange={(e) => handleInputChange(e, node)}
             />
+            {/* <input
+              type="text"
+              value={node.value}
+              onChange={(e) => handleInputChange(e, node)}
+            /> */}
             <br />
           </div>
         );
@@ -67,8 +100,13 @@ export const XmlForm = () => {
   return (
     <>
       <div>{renderForm(modifiedXML)}</div>
-      <button onClick={handleSave}>Guardar</button>
-      <button onClick={handleDownload}>Descargar</button>
+      <AddCrossmark onSave={handleAddCrossmark} />
+      <Button variant="info" onClick={handleSave} style={{ margin: "10px" }}>
+        Guardar
+      </Button>
+      <Button variant="success" onClick={handleDownload}>
+        Descargar
+      </Button>
     </>
   );
 };
